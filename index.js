64ee00f7f7d4d4acb945853e8fc70dfd5a367d86
Tristen Allen <spyboticsguy@gmail.com>
2018-03-12 23:11:03 -0400

    Rearrange index.js for more logical progression.

--- index.js
+++ index.js
@@ -6,17 +6,16 @@ var routes = require('./routes/headlineRoutes')
 var app = express()
 var port = process.env.PORT || 3000
 
+// set up template engine
+app.set('view engine', 'pug')
+app.listen(port)
+
 // set up request parsers
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 // serve public files
 app.use(express.static('public'))
-
-// set up template engine
-app.set('view engine', 'pug')
-
-app.listen(port)
 
 // register routes
 routes(app)
